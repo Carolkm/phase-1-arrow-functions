5e8d101eae080123153d78ccc9f50e90f1efa038
Caroline kahuro <carolkahuro@gmail.com>
2023-07-19 12:04:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+//it("has a function expression called divide", () => {
+    
+    const divide = function () {
+       return 20 
+    }
+
+    //it("has an arrow function called square", () => {
+        //expect(square).to.exist
+
+    const square = (x) => x**2
+    let x=2
+// it("add arrow function takes two parameters and adds them together", () => {
+    //let a = 3
+    //let b = 4
+    const add = (a,b) => a+b
+    let a=3
+    let b=4
+    
+
